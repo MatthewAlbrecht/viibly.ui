2a66943ba17b62d4aad4c958d131c29c715d64a5
Albrecht <malbrech@MALBRECHMAC-HO.local>
2019-10-15 09:16:24 -0400

    ADDED: Links on every page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
   render() {
     return (
       <>
         <h1>Home Page</h1>
+        <ul>
+          <li>
+            <Link to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/login">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/albums">
+              Albums
+            </Link>
+          </li>
+          <li>
+            <Link to="/about-us">
+              About Us
+            </Link>
+          </li>
+        </ul>
       </>
     );
   }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Login extends Component {
   render() {
     return (
       <>
         <h1>Login Page</h1>
+        <ul>
+          <li>
+            <Link to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/login">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/albums">
+              Albums
+            </Link>
+          </li>
+          <li>
+            <Link to="/about-us">
+              About Us
+            </Link>
+          </li>
+        </ul>
       </>
     );
   }
