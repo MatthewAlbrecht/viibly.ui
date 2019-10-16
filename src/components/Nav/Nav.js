ea8b1359c24ae7e8b2c2195d4b868f59dea7a55e
Matthew Albrecht <MatthewAlbrecht@users.noreply.github.com>
2019-10-16 10:26:11 -0400

    VIB-15: Nav (#11)
    * CHANGE: scss vars to camelcase
    
    * MOVED: nav into header
    
    * ADDED: nav component
    
    * FIX: pixel usage and classnames usage
    
    * UPDATE: packages to latest version

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { changeNavTransparency } from 'actions/nav';
+import classnames from 'classnames'
+
+function Nav(props) {
+  console.log(props);
+
+  const navClasses = classnames({
+    'nav': true,
+    'nav_transparent': props.transparency,
+  })
+
+  return (
+    <nav className={navClasses}>
+      <h1 
+        className="nav-logo"
+        onClick={() => props.changeNavTransparency(!props.transparency)}
+      >Logo</h1>
+      <div className="hamburgerMenu">
+        <span className="hamburgerMenu-line"></span>
+        <span className="hamburgerMenu-line"></span>
+      </div>
+    </nav>
+  );
+}
+
+const mapStateToProps = state => ({
+  ...state.nav,
+});
+
+/**
+ * Remove mapDispacthToProps once we find a new place for it
+ */
+const mapDispatchToProps = dispatch => ({
+  changeNavTransparency: (transparency) => dispatch(changeNavTransparency(transparency)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
