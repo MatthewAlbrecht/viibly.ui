b5c85efc476342ffb113b6430ac405d72070533b
Albrecht <malbrech@MALBRECHMAC-HO.local>
2019-10-15 20:54:56 -0400

    MOVED: nav into header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Nav from 'components/Nav/Nav'
 
 export default function Header() {
   return (
     <header className="header">
-      THIS IS THE HEADER
+      <Nav></Nav>
     </header>
   );
 }
