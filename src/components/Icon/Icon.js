00483b5aae5edbf1cb240f43eb75e6c3c78cc2b1
Albrecht <malbrech@MALBRECHMAC-HO.local>
2019-10-17 09:42:00 -0400

    UPDATE: icon to take classes and add footerswoosh

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Info from './Icons/Info';
+import FooterSwoosh from './Icons/FooterSwoosh';
+import classnames from 'classnames';
 
 export default function Icon(props) {
+  function getClasses() {
+    return props.classes && props.classes.split(" ").map(iconClass => `icon_${iconClass}`);
+  }
+
+  function getClassName() {
+    return classnames(
+      !props.notIcon && 'icon',
+      getClasses(),
+      props.className,
+    );
+  }
+
   const iconMap = {
     'Info': <Info/>,
+    'FooterSwoosh': <FooterSwoosh/>,
   };
 
   return (
-    <i className={props.className}>
+    <i className={getClassName()}>
       {iconMap[props.type]}
     </i>
   );
