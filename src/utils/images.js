e36b3e4799f353b207a5af08e48ce6ef2ebea2d9
Albrecht <malbrech@MALBRECHMAC-HO.local>
2019-10-18 07:24:48 -0400

    ADDED: boiler album photo

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,7 +1,11 @@
 import helmet from 'assets/images/helmet.png';
+import albumCover from 'assets/images/albumCover.png';
 
 export default {
   'helmet': {
     'src': helmet,
+  },
+  'albumCover': {
+    'src': albumCover,
   },
 };
